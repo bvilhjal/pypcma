2e7842fd3f2521e20fd22e3a6d4c099e34f2bf97
Bjarni Jóhan Vihjálmsson <bjarni.vilhjalmsson@gmail.com>
2016-03-02 11:25:28 +0100

    workin on the cluster

--- analyze_results.py
+++ analyze_results.py
@@ -632,5 +632,5 @@ if __name__=='__main__':
 #                    fig_filename='/Users/bjarnivilhjalmsson/data/tmp/ps_MVT_WC.png', method='MVT', 
 #                    ylabel='MVT (HIP,WC,HGT,BMI) $-log_{10}(P$-value$)$', xlabel='WC $-log_{10}(P$-value$)$')
 
-#     run_t('/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zs_ldprunedno_weights.txt','/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zsno_weights.txt', 'GIANT2', '/home/bjarni/PCMA/faststorage/2_RESULTS/GIANT2')
-    count_ld_indep_regions('/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zs.txt', '/home/bjarni/PCMA/faststorage/2_RESULTS/PCMA_GIANT2_t1.0.txt', ld_reg_map = '/project/PCMA/faststorage/1_DATA/fourier_ls.hdf5')
+    run_all_ts('/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zs_ldprunedno_weights.txt','/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zsno_weights.txt', 'GIANT2', '/home/bjarni/PCMA/faststorage/2_RESULTS/GIANT2')
+    count_ld_indep_regions('/home/bjarni/PCMA/faststorage/1_DATA/GIANT_2_zs.txt', '/home/bjarni/PCMA/faststorage/2_RESULTS/PCMA_GIANT2_t0.6.txt', ld_reg_map = '/project/PCMA/faststorage/1_DATA/fourier_ls.hdf5')
