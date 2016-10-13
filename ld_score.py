f20ee5618522b8b544417e7617b607526d711b7d
Bjarni Vilhjalmsson <bjarni.vilhjalmsson@gmail.com>
2016-10-13 08:47:47 +0200

    bla

--- ld_score.py
+++ ld_score.py
@@ -145,7 +145,7 @@ def pre_calculate_everything(input_genotype_file, pca_adj_ld_score_file, ld_scor
         print 'Working on Chromosome %d' % chrom
         chrom_str = 'chr%d' % chrom
         K = kinship_pca_dict[chrom_str]['K_leave_one_out']
-        T = linalg.cholesky(linalg.pinv())
+        T = linalg.cholesky(linalg.pinv(K))
         assert T.shape == K.shape
         print T.shape
         chrom_snp_trans_mats[chrom_str] = T
